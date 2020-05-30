26d493d1999fc8d9e89b291bb104b0b3cacc8387
ljx_glb <1913154305@qq.com>
2020-05-30 21:32:56 +0800

    poland calculate complete

--- src/com/liujx/Poland.java
+++ src/com/liujx/Poland.java
@@ -1,9 +1,76 @@
 package com.liujx;
 
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 public class Poland {
+
+    public Queue<String> toInFix(String func){
+        List<String> translate = translate(func);
+
+        Stack<String> s1 = new Stack<>();
+        Queue<String> result = new ArrayDeque<>();
+
+
+
+        for (String str : translate){
+            if (str.matches("\\d+")){
+                result.add(str);
+            }else if (")".equals(str)){
+                while (!"(".equals(s1.peek())){
+                    result.add(s1.pop());
+                }
+                s1.pop();
+            }else if ("(".equals(str)){
+                s1.push(str);
+            }else{
+                if (s1.empty()){
+                    s1.push(str);
+                }else{
+                    while (!s1.empty() && !"(".equals(s1.peek()) && getWeight(str.charAt(0)) <= getWeight(s1.peek().charAt(0))){
+                        result.add(s1.pop());
+                    }
+                    s1.push(str);
+
+                }
+            }
+        }
+
+        while (!s1.empty()){
+            result.add(s1.pop());
+        }
+
+        return result;
+    }
+
+    public List<String> translate(String func){
+        List<String> list = new ArrayList<>();
+        String realNum = "";
+        for (int i = 0; i < func.length(); i++){
+            char c = func.charAt(i);
+            if (isOpt(c)){
+                list.add(String.valueOf(c));
+            }else{
+                realNum += c;
+                if (i == func.length() - 1 || isOpt(func.charAt(i + 1))){
+                    list.add(realNum);
+                    realNum = "";
+                }
+            }
+        }
+        return list;
+    }
+
+    public boolean isOpt(int data){
+        return data < 48 || data > 57;
+    }
+
+    public int getWeight(int data){
+        if (data == '+' || data == '-'){
+            return 1;
+        }else{
+            return 2;
+        }
+    }
 
     public int polandCalculate(String func){
         String[] array = func.split(" ");
@@ -59,9 +126,16 @@ public class Poland {
 
     public static void main(String[] args) {
         Poland poland = new Poland();
-        int calculate = poland.polandCalculate("- * + 3 4 50 6 ");
-        int result = poland.rePolandCalculate("3 4 + 50 * 6 -");
-        System.out.println(calculate + ", " + result);
-        System.out.println(calculate == result);
+        List<String> list = poland.translate("(30+4)*55-66");
+        System.out.println(list);
+        Queue<String> queue = poland.toInFix("1+2*3/4");
+        StringBuilder func = new StringBuilder();
+        while (!queue.isEmpty()){
+            func.append(queue.poll() + " ");
+        }
+        System.out.println(func);
+
+        int calculate = poland.rePolandCalculate(func.toString());
+        System.out.println(calculate);
     }
 }
